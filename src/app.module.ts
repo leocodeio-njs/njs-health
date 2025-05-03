76a92aadedf057b8f60605e262f91abfa86efb57
leocodeio <basicleo787@gmail.com>
2025-05-03 09:34:32 +0530

    perf(moved response and performance to logging): moved response and performance to logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
-import { PerformanceInterceptor } from './interceptors/performance.interceptor';
-import { ResponseInterceptor } from './interceptors/response.interceptor';
 import { PrometheusService } from './health/prometheus.service';
 
 @Module({
   imports: [HealthModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    PrometheusService,
-    {
-      provide: 'APP_INTERCEPTOR',
-      useClass: PerformanceInterceptor,
-    },
-    {
-      provide: 'APP_INTERCEPTOR',
-      useClass: ResponseInterceptor,
-    },
-  ],
+  providers: [AppService, PrometheusService],
 })
 export class AppModule {}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 import { HealthModule } from './health/health.module';
-import { PerformanceInterceptor } from './interceptors/performance.interceptor';
-import { ResponseInterceptor } from './interceptors/response.interceptor';
 
-export { HealthModule, PerformanceInterceptor, ResponseInterceptor };
+export { HealthModule};
